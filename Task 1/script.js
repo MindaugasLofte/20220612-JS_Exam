6e56007216df254635fe1dbbecc6827ebed098d3
Mindaugas Grikšelis <mindusis@gmail.com>
2022-07-12 00:09:18 +0300

    1 uzduotis atlikta

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,47 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+
+function kgToLb() { lb = inpt.value * 2.2046; return lb.toFixed(2) }
+
+function kgToG() { g = inpt.value / 0.0010000; return g.toFixed(2) }
+
+function kgToOz() { oz = inpt.value * 35.274; return oz.toFixed(2) }
+
+let btn = document.querySelector('#submit-btn');
+let inpt = document.querySelector('#search');
+let output = document.querySelector('#output');
+let title = document.getElementById('title');
+
+btn.addEventListener('click', printOutput);
+
+function printOutput() {
+    event.preventDefault()
+
+    if (isNaN(inpt.value)) {
+        return alert(inpt.value + ' is not a NUMBER! Please give me a Number next time:) please!');
+    }
+    output.innerHTML = `
+    <div class="style">
+    <div><h3>Jūsų įvesti ${inpt.value}kg sudaro: </h3></div>
+    <div>
+    ${kgToLb()}lb 
+    ARBA ${kgToG()}g 
+    ARBA ${kgToOz()}oz</div>
+    </div>`
+    document.querySelector(".style").style.cssText = `
+  display: block; 
+  position: static;
+  color:white;
+  font-size:30px;
+  border:2px solid black;
+  border-radius:15%
+  padding: 20px;
+  text-align: center;
+  size:50vh;
+  background-color:SlateBlue;
+  border-radius: 10px;
+  width:60%;
+  margin: auto;
+`
+}
