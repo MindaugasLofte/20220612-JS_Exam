adb7fc1194c738bfc8e73f842b4545aaeac2ce1b
Mindaugas Grikšelis <mindusis@gmail.com>
2022-07-12 17:48:39 +0300

    9 uzduotis atlikta

--- Task 9/script.js
+++ Task 9/script.js
@@ -9,3 +9,17 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+class Movie {
+    constructor(title, director, budget) {
+        this.title = title;
+        this.director = director;
+        this.budget = budget;
+    }
+}
+const filmas = new Movie("Titanikas", "Džordana", 200000000);
+if (filmas.budget > 100000000 ? (filmas.wasExpensive = "true") : (filmas.wasExpensive = "false"));
+// console.log(filmas.prototype)
+
+// perduoti atsakymą į console
+console.log("Atsakymas: ",
+    filmas)
