9842a30c044cf75dc645383504edb79e57293956
Mindaugas Grikšelis <mindusis@gmail.com>
2022-07-12 14:48:30 +0300

    8 uzduotis atlikta

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,60 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+// Constructor function for Calculator objects
+class Calculator {
+    constructor(firstNumber, secondNumber) {
+        this.firstNumber = firstNumber;
+        this.secondNumber = secondNumber;
+        this.sum = function() {
+            return this.firstNumber + this.secondNumber;
+        };
+        this.subtraction = function() {
+            return this.firstNumber - this.secondNumber;
+        };
+        this.multiplication = function() {
+            return this.firstNumber * this.secondNumber;
+        };
+        this.division = function() {
+            return (this.firstNumber / this.secondNumber).toFixed(3);
+        };
+    }
+}
+// Create a Calculator object
+// const numbers = new Calculator(15, 7);
+
+
+// Create a Calculator object. Additional option: User must give two numbers
+const numbers = new Calculator(parseInt(prompt("Įveskite pirmą skaičių")), parseInt(prompt("Įveskite pirmą skaičių")));
+
+// Display output
+let output = `Sum(a+b)= ${numbers.sum()},
+    Subtraction(a-b)= ${numbers.subtraction()},
+    Multiplication(a*b)= ${numbers.multiplication()},
+    Division(a / b) = ${ numbers.division()}`
+
+// papildomai sukuriamas naujas DIV elementas html->body dalyje.
+let newDiv = document.createElement('div');
+const newDivText = document.createTextNode("");
+newDiv.style.textAlign = "center";
+newDiv.style.display = "block";
+newDiv.style.color = "magenta";
+newDiv.appendChild(newDivText);
+document.querySelector("body").appendChild(newDiv);
+
+// peruoti atsakymą į html
+document.querySelector("div").innerHTML = "<h2>Atsakymas:</h2>" + output;
+document.querySelector("div").style.cssText = `
+      color:white;
+      font-size:30px;
+      border:2px solid black;
+      padding: 20px;
+      text-align: center;
+      size:50vh;
+      background-color:SlateBlue;
+      border-radius: 10px;
+      width:60%;
+      margin: auto;
+    `
+    // perduoti atsakymą į console
+console.log("Atsakymas:", output)
